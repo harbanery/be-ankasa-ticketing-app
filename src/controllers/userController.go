e665175e5d6ca08dfb2c8115f3f844efd4f1f44c
harbanery <ryusuf05@gmail.com>
2024-08-31 12:14:47 +0700

    feat 1: register without merchant temp

--- src/controllers/userController.go
+++ src/controllers/userController.go
@@ -103,20 +103,22 @@ func RegisterUser(c *fiber.Ctx) error {
 		})
 	}
 
-	if newUser.Role == "merchant" {
-		newMerchant := models.Merchant{
-			UserID: userID,
-			Name:   user.Username,
-		}
+	// if newUser.Role == "merchant" {
+	// 	newMerchant := models.Merchant{
+	// 		UserID: userID,
+	// 		Name:   user.Username,
+	// 	}
 
-		if err := models.CreateMerchant(&newMerchant); err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"status":     "server error",
-				"statusCode": 500,
-				"message":    "Failed to create merchant",
-			})
-		}
-	} else if newUser.Role == "customer" {
+	// 	if err := models.CreateMerchant(&newMerchant); err != nil {
+	// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+	// 			"status":     "server error",
+	// 			"statusCode": 500,
+	// 			"message":    "Failed to create merchant",
+	// 		})
+	// 	}
+	// } else
+
+	if newUser.Role == "customer" {
 		newCustomer := models.Customer{
 			UserID:   userID,
 			Username: user.Username,
